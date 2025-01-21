530b57749d616265388508f51fa559b38fd7adce
IsmaCeva11os <carlos.cevallos06@epn.edu.ec>
2025-01-21 14:53:17 -0500

    Add files via upload

---
+++ cubo de número.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main () {
+	float numero, cubo;
+	cout << "ingresa un numero:" ;
+		cin >> numero ;
+	cubo= numero * numero * numero;
+	cout <<"El cubo de " << numero << "es el siguiente " <<  cubo << endl;
+	return 0;
+}
+
